d673ac60a1783b47fd67be5fd5c941cad715ca6e
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-21 19:22:33 -0700

    changes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+console.log(inquirer)
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
+//fs.writeFile('./index.html', pageHTML, err => {
+  //  if (err) throw err;
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
